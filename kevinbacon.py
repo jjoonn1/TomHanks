ce6faad0217064d9cb37af5c78708f3088de30dd
Jonathan Feigenson <jonathan@swimm.io>
2022-10-24 10:59:05 +0300

    DSDSDS

--- kevinbacon.py
+++ kevinbacon.py
@@ -8,19 +8,10 @@ from threading import Thread
 
 
 class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
-        self._return = None
-
-    def run(self):
-        if self._Thread__target is not None:
-            self._return = self._Thread__target(*self._Thread__args,
-                                                **self._Thread__kwargs)
-
-    def join(self):
-        Thread.join(self)
-        return self._return
+    def __init__(self, group=None, target=None, name=None):
+        print ('all dsdas')
+    def yo():
+        print(ds'sds');
 
 
 def get_links_for_movies():
